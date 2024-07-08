480c58719ae2b7661e7034db8dde7ee34f55d94a
rootiest <chris@rootiest.com>
2024-07-08 15:18:01 -0400

    🗜️ Abreviations
    - Add initial batch of abreviations
    - Convert several functions to abbreviations

---
+++ conf.d/abbr.fish
@@ -0,0 +1,14 @@
+### Abreviations ###
+
+abbr -a n nvim
+abbr -a v code
+abbr -a k kate
+abbr -a s kitten ssh
+abbr -a d kitten diff
+abbr -a kdiff kitten diff
+abbr -a diff kitty +kitten diff
+abbr -a editt kitty @ launch --type=tab nvim
+abbr -a hg kitten hyperlinked_grep
+abbr -a icat kitten icat
+abbr -a lg lazygit
+abbr -a sudu sudo -s
--- functions/gitui.fish
+++ functions/gitui.fish
@@ -1,4 +1,4 @@
 function gitui --description 'alias gitui=gitui -t mocha.ron'
- command gitui -t mocha.ron $argv
-        
+    command gitui -t frappe.ron $argv
+
 end
