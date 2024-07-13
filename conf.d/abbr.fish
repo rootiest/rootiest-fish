f98c6cac38369cb8330ea90e664f85fce6edc510
rootiest <chris@rootiest.com>
2024-07-13 16:07:47 -0400

    🪄 Abreviations
    - Add more abbreviations and shortcuts
    - Document them with comments

--- conf.d/abbr.fish
+++ conf.d/abbr.fish
@@ -1,24 +1,60 @@
 ### Abreviations ###
 
+# Neovim
 abbr -a n nvim
+abbr -a neovim nvim
+# VSCode
 abbr -a v code
+# Kate
 abbr -a k kate
+# Kitty Kitten SSH
 abbr -a s kitten ssh
+# Kitty Kitten Diff
 abbr -a d kitten diff
 abbr -a kdiff kitten diff
 abbr -a diff kitty +kitten diff
+# Neovim in a new tab
 abbr -a editt kitty @ launch --type=tab nvim
+# Kitty Kitten hyperlinked grep
 abbr -a hg kitten hyperlinked_grep
+# Kitty Kitten icat
 abbr -a icat kitten icat
+# LazyGit
 abbr -a lg lazygit
+# Sudo shell
 abbr -a sudu sudo -s
+# Kitty
 abbr -a kt kitty
+# cat
 abbr -a c cat
+# chezmoi
 abbr -a cm chezmoi
 abbr -a cz chezmoi
+# chezmoi cd
 abbr -a cmcd chezmoi cd
 abbr -a czcd chezmoi cd
-abbr -a g git
+# chezmoi edit
+abbr -a cme chezmoi edit
+abbr -a cze chezmoi edit
+# chezmoi add
+abbr -a cmad chezmoi add
+abbr -a czad chezmoi add
+# chezmoi apply
+abbr -a cmap chezmoi apply
+abbr -a czap chezmoi apply
+# chezmoi rm
+abbr -a cmrm chezmoi forget
+abbr -a cmf chezmoi forget
+abbr -a czrm chezmoi forget
+abbr -a czf chezmoi forget
+# chezmoi init
+abbr -a cmi chezmoi init
+abbr -a czi chezmoi init
+# Edit
 abbr -a e edit
+# Sudoedit
 abbr -a se sudoedit
+# Git
+abbr -a g git
+# Quit
 abbr -a :q exit
