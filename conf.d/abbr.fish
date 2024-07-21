15ae68f884fd27f2f00f45e04b1ea1b429887804
rootiest <chris@rootiest.com>
2024-07-20 23:09:06 -0400

    Add kitty shortcuts
    - Add several vim-cmd style shortcuts for various kitty operations

--- conf.d/abbr.fish
+++ conf.d/abbr.fish
@@ -61,4 +61,18 @@ abbr -a se sudoedit
 # Git
 abbr -a g git
 # Quit
-abbr -a :q exit
+abbr -a :q kitty @ close-window
+abbr -a :Q kitty @ close-tab
+# Window Creation
+abbr -a :w kitty @ new-window
+abbr -a :wv kitty @ launch --location=vsplit
+abbr -a :wh kitty @ launch --location=hsplit
+#Window Detatch
+abbr -a :wo kitty @ detach-window
+abbr -a :wot kitty @ detach-window --target=tab
+# Tab Management
+abbr -a :t kitty @ launch type=tab
+abbr -a :to kitty @ detach-tab
+# Tab Navigation
+abbr -a :tp kitty @ action goto_tab 0
+abbr -a :tn kitty @ action goto_tab 1
